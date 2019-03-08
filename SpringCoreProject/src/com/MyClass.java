c1c132edfeed6af9e7139513dc89c185b14e6f05
user <user@LAPTOP-49MH6DJS>
2019-03-08 08:51:02 +0530

    ok

--- SpringCoreProject/src/com/MyClass.java
+++ SpringCoreProject/src/com/MyClass.java
@@ -3,7 +3,7 @@ package com;
 public class MyClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("amit kumar");
 
 	}
 
--- SpringCoreProject/src/com/Simple.java
+++ SpringCoreProject/src/com/Simple.java
@@ -4,6 +4,7 @@ public class Simple {
 
 	public static void main(String[] args) {
 		System.out.println("thanks");
+		System.out.println("amit kumar");
 
 	}
 
