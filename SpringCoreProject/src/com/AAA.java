34ba914260649519de050d79444c2b1866b097a7
amitkumarakshat <43228754+amitkumarakshat@users.noreply.github.com>
2020-09-01 09:31:57 +0530

    Update AAA.java

--- SpringCoreProject/src/com/AAA.java
+++ SpringCoreProject/src/com/AAA.java
@@ -4,12 +4,7 @@ public class AAA {
    public static void main(String args[]){
 	   System.out.println("welcome to my home");
 
-	   /* comment*/
-
-	   /* ok got it */
-	   /*amit kumar*/	  
-	   System.out.println("1");
-	   System.out.println("2");
+	  
 
 
    }
