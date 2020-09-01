f7910437f8d5a88c7e589d65787081f1202aa15c
amitchoudhary02 <amitchoudhary02@amitchoudhary02.mshome.net>
2020-09-01 09:43:35 +0530

    ok

--- SpringCoreProject/src/com/Simple.java
+++ SpringCoreProject/src/com/Simple.java
@@ -1,14 +1,11 @@
 package com;
 
-public class Simple1 {
+public class Simple {
 
 	public static void main(String[] args) {
 		System.out.println("thanks");
 		System.out.println("amit kumar");
-		
-		
-		int a = 0;
-		int b = 0;
+
 	}
 
 }
